0fd9f1b83d36ab48b70cd55a53abce1d004a8c69
steveruu <axo@ax4.cz>
2026-02-05 08:26:49 +0100

    feat: init

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MOT_RazorPage.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
